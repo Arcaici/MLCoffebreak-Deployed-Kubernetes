9cdd76542c2d3df10cb10725cb6d1c68bfafb556
Arcaici <43611155+Arcaici@users.noreply.github.com>
2023-04-12 11:33:00 +0200

    second version of inference model

--- model/infer.py
+++ model/infer.py
@@ -27,27 +27,27 @@ def clean_drink(drink):
     drink = make_lowercase(drink)
     drink = remove_special_chars(drink)
     return drink
+def preprocessing(new_data, tfidf_enc):
+    new_data.drink = clean_drink(new_data.drink)
+    drinks_word_features = tfidf_enc.transform(new_data.drink)
+    drinks_word_features = pd.DataFrame(drinks_word_features.toarray().transpose(),
+                                        index=tfidf_enc.get_feature_names())
+    new_data.drop(columns="drink", axis=1, inplace=True)
+    new_data = pd.concat([new_data, drinks_word_features.transpose()], axis=1)
+    return new_data
+if __name__ == '__main__':
+    #loading model, label encoder, TfidfVectorizer
+    le = pickle.load(open('label_enc.pkl', 'rb'))
+    tfidf_enc = pickle.load(open('tfidf_enc.pkl', 'rb'))
+    clf = pickle.load(open('model.pkl', 'rb'))
 
-#loading model, label encoder, TfidfVectorizer
-le = pickle.load(open('label_enc.pkl', 'rb'))
-tfidf_enc = pickle.load(open('tfidf_enc.pkl', 'rb'))
-clf = pickle.load(open('model.pkl', 'rb'))
+    #input data
+    new_data = pd.DataFrame(data= np.array([["Celsius Energy Drink", 354.882, 10, 200, 0.563568]]), columns=['drink', 'volume', 'calories', 'caffeine', 'caffeine_over_ml'])
+    new_data = preprocessing(new_data, tfidf_enc)
 
-#input data
-new_data = pd.DataFrame(data= np.array([["Celsius Energy Drink", 354.882, 10, 200, 0.563568]]), columns=['drink', 'volume', 'calories', 'caffeine', 'caffeine_over_ml'])
-print(new_data)
-
-#preprocessing
-new_data.drink = clean_drink(new_data.drink)
-drinks_word_features=tfidf_enc.transform(new_data.drink)
-drinks_word_features = pd.DataFrame(drinks_word_features.toarray().transpose(),
-                   index=tfidf_enc.get_feature_names())
-new_data.drop(columns = "drink" ,axis=1, inplace=True)
-new_data = pd.concat([new_data, drinks_word_features.transpose()], axis=1)
-
-#prediction
-predicted = clf.predict(new_data)
-print(le.inverse_transform(predicted))
+    #prediction
+    predicted = clf.predict(new_data)
+    print(le.inverse_transform(predicted))
 
 # drink               Celsius Energy Drink
 # volume                           354.882
